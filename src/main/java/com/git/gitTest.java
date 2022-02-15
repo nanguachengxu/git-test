cab03ece3ab21356a3cfee64802552ee574096d7
Xie <2945377116@qq.com>
2022-02-15 15:37:48 +0800

    hot-fix commit

--- src/main/java/com/git/gitTest.java
+++ src/main/java/com/git/gitTest.java
@@ -4,5 +4,7 @@ public class gitTest {
     public static void main(String[] args) {
         System.out.println("你好,git");
         System.out.println("hello.git");
+        System.out.println("new git hello");
+        System.out.println("hi git");
     }
 }
